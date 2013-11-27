9ed74fd9a0514a6efefc15a26d5434931b22d995
Patrick <PC@11.1.11.221>
2013-11-27 13:42:20 -0800

    Removed browsequiz.jsp and updated SiquoiaFrontController to handle ajax calls

--- SiquoiaQuizProject/src/com/siquoia/view/SiquoiaFrontController.java
+++ SiquoiaQuizProject/src/com/siquoia/view/SiquoiaFrontController.java
@@ -1,13 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.siquoia.view;
 
+import com.siquoia.command.AjaxCommand;
 import com.siquoia.command.Command;
-import com.siquoia.command.TargetCommand;
 import com.siquoia.exception.CommandException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -24,21 +23,52 @@ public class SiquoiaFrontController extends HttpServlet {
 
     @Override
     public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("application/json; charset=utf-8");
+        req.setCharacterEncoding("utf-8");
         String commandKey = req.getParameter("command");
         Command command = CommandFactory.getInstance().findCommand(commandKey);
-        String target = null;
-        try {
-        	
-            target = command.execute(req);
-            System.out.println("---test"+target);
-        } catch (CommandException ce) {
-            req.setAttribute("cause", ce.getCause());
-            req.setAttribute("message", ce.getMessage());
-            req.setAttribute("stackTrace", ce.getStackTrace());
-            RequestDispatcher dispatcher = req.getRequestDispatcher(ce.getErrorTarget());
-            dispatcher.forward(req, res);
+        if(command == null){
+            ajaxCall(req, res, commandKey);
         }
+        else{
+            String target = null;
+            try {
+                target = command.execute(req);
+            } catch (CommandException ce) {
+                req.getSession().removeAttribute("loggedIn");
+                req.setAttribute("cause", ce.getCause());
+                req.setAttribute("message", ce.getMessage());
+                req.setAttribute("stackTrace", ce.getStackTrace());
+                RequestDispatcher dispatcher = req.getRequestDispatcher(ce.getErrorTarget());
+                dispatcher.forward(req, res);
+            }catch(NullPointerException npe){
+                req.getSession().removeAttribute("loggedIn");
+                req.setAttribute("message", "No command found");
+            }
+            req.getRequestDispatcher(target).forward(req, res);
+        }
+    }
+    
+    private void ajaxCall(HttpServletRequest req, HttpServletResponse res, String commandKey) throws ServletException, IOException{
+            
+            PrintWriter out = res.getWriter();
+            AjaxCommand command = CommandFactory.getInstance().findAjaxCommand(commandKey);
 
-        req.getRequestDispatcher(target).forward(req, res);
+            try{
+                out.println(command.execute(req, ""));
+                System.out.println(command.execute(req, ""));
+            } catch (CommandException ex) {
+                req.getSession().removeAttribute("loggedIn");
+                req.setAttribute("cause", ex.getCause());
+                req.setAttribute("message", ex.getMessage());
+                req.setAttribute("stackTrace", ex.getStackTrace());
+                RequestDispatcher dispatcher = req.getRequestDispatcher(ex.getErrorTarget());
+                dispatcher.forward(req, res);
+            }catch(NullPointerException npe){
+                req.setAttribute("message", "Couldn't execute command");
+                out.println("blah");
+            }finally{
+                out.close();
+            }
     }
 }
